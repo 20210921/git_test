cdeb8d62633bdf80346455000891d8efcb6cc7d1
tom <goodMorning@163.com>
2022-10-03 15:22:01 +0800

    hot-fix commit

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -2,5 +2,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,fix");
+        System.out.println("hello,fix2");
     }
 }
