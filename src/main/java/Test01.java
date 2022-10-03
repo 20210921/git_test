56aff8a8c015e842f99b5176a3066fdd753a35b8
Vegetable Bird <91109551+20210921@users.noreply.github.com>
2022-10-03 15:56:15 +0800

    Update Test01.java

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("hello,fix2");
         System.out.println("master");
         System.out.println("fix ");
+        System.out.println("修改");
     }
 }
